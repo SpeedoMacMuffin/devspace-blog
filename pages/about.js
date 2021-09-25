11a77e96d9ab435395cc88b005a9af43e248edb9
Stefan Klarmann <stefan@fddb.de>
2021-09-25 17:33:55 +0200

    removed scrollbar

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ export default function AboutPage() {
     return (
         <Layout title='About DevSpace'>
             <h1 className='text-5xl border-b-4 pb-5 font-bold mx-5'>About</h1>
-            <div className='bg-white shadow-md rounded-lg px-10 py-6 mt-6'>
+            <div className='bg-white shadow-md rounded-lg px-10 py-6 mt-6 mx-5'>
                 <h3 className="text-2xl mb-5">DevSpace Blog</h3>
                 <p className="mb-3">This is a Blog build with Next.js and Markdown</p>
                 <p>
